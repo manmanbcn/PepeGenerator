2c1360995c190b377dbb46a23f0637f33678ab54
manmanbcn <141034457+manmanbcn@users.noreply.github.com>
2023-11-21 16:31:04 +0100

    Buds fixed

--- src/decal.js
+++ src/decal.js
@@ -1,8 +1,8 @@
 const decalData = [
-    { id: 1, name: 'Unknown_pleasures', url: '/src/assets/decal/Unknown_pleasures.png' },
-    { id: 2, name: 'Radiohead', url: '/src/assets/decal/Radiohead.png' },
-    { id: 3, name: 'Moon', url: '/src/assets/decal/Moon.png' },
-    { id: 4, name: 'PulpFiction', url: '/src/assets/decal/PulpFiction.png' },
+    { id: 1, name: 'Unknown_pleasures', url: 'src/assets/decal/Unknown_pleasures.png' },
+    { id: 2, name: 'Radiohead', url: 'src/assets/decal/Radiohead.png' },
+    { id: 3, name: 'Moon', url: 'src/assets/decal/Moon.png' },
+    { id: 4, name: 'PulpFiction', url: 'src/assets/decal/PulpFiction.png' },
     { id: 5, name: 'Pacific_Pizza', url: 'src/assets/decal/Pacific_Pizza.png' },
     { id: 6, name: 'Ingsoc', url: 'src/assets/decal/Ingsoc.png' },
     { id: 7, name: 'Evangeleon', url: 'src/assets/decal/Evangeleon.png' },
